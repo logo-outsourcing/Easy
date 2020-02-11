ea09431d065b187f2c1d80f53be438b0f0fe1d7b
= <vikytrusowa.@gmail.com>
2020-02-11 21:18:51 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.brands__slider.swiper-container', {
+    mode: 'horizontal',
+    slidesPerView: 5,
+    pagination: false,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    autoplay: 2500,
+    loop: true
+});
